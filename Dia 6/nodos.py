9d98f0b8def1d9981b3a0cf2aaebaa5c640b3bb9
JesusBustamante <160262827+JesusBustamante@users.noreply.github.com>
2024-04-16 12:59:13 -0500

    Python Dia 6

---
+++ Dia 6/nodos.py
@@ -0,0 +1,11 @@
+nodos = []
+cantidad = int(input("cuantos valores vas a ingresar "))
+assert 0 <= cantidad <= 300, "Error, escriba números en rango de 0 _ 300"
+
+for i in range(0, cantidad):
+    nodos.append(input("Ingrese los valores de la lista "))
+
+duplicados = list(set(nodos))
+
+duplicados.sort()
+print(duplicados)
